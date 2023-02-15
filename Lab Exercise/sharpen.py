719ba6c1fdb61a7237124e63b1a37f91e29555be
Hardin-J <105577218+Hardin-J@users.noreply.github.com>
2023-02-15 15:44:20 +0530

    Add files via upload

---
+++ Lab Exercise/sharpen.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+original_image = cv2.imread('j1.jpg')
+cv2.imshow('original image',original_image)
+cv2.waitKey(0)
+
+sharpen_filter=np.array([[-1,-1,-1],
+                         [-1,9,-1],
+                         [-1,-1,-1]])
+
+sharp_image=cv2.filter2D(original_image,-1,sharpen_filter)
+cv2.imshow('Required image',sharp_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
